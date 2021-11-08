62c343ce97baf3dbb68b2b4991dceef80e6d164b
philippe <kanengelephilippe@gmail.com>
2021-11-08 22:43:01 +0100

    methode

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,63 @@
-
-let touches = Array.from(document.querySelectorAll(".boutton"));
-console.log(touches);
-let ecran = document.querySelectorAll(".ecran");
+let buttons = Array.from(document.querySelectorAll(".boutton"));
+console.log(buttons);
+let ecran = document.querySelector("#ecran");
 console.log(ecran);
 
-touches.map(button =>{
-    button.addEventListener("click",(e)=>{
 
-        const valeur = e.target
-        calculer(valeur);
+buttons.map(boutton => {
+    boutton.addEventListener("click", (e) => {
+        switch (e.target.innerText) {
+            case "c":
+                ecran.innerText = "";
+
+                break;
+            case "=":
+                // const calcul = eval(ecran.textContent);
+                // ecran.textContent = calcul;
+                console.log(ecran.textContent);
+
+                break;
+
+            default:
+                ecran.innerText += e.target.innerText;
+                ecran.style.color = "white"
+
+                break;
+        }
 
     })
 
 })
 
 
+let operator = ["+", "-", "*", "/"];
+let operatorFound;
+operator.forEach(el => {
+    el.indexOf([operator]);
+
+    switch (operator) {
+        case "+":
+            ecran
 
 
+            break;
+        case "-":
+
+            break;
+        case "*":
+
+            break;
+        case "/":
+
+            break;
+        
 
 
+        default:
+            break;
+    }
 
-    
+});
+// let a = ecran.addEventListener("click",()=>{
+//     ecran.innerText = operator;    
+// })
